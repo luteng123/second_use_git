879595486a052f69e0e33f67402e4972dd3ad085
luteng <542170624@qq.com>
2016-04-26 16:29:15 +0800

    000

--- app/src/main/java/com/goyo/externalstoragetest/MainActivity.java
+++ app/src/main/java/com/goyo/externalstoragetest/MainActivity.java
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
     public void btnWriteExternalStorage(View view) {
